b14d5167e3e2c15c6b5adf7728e72c07d2dc9ea3
nargyanti <nargyanti@gmail.com>
2022-04-16 14:58:14 +0700

    [Week 3] Creating stateless & stateful component

--- week-2/web-reactku/src/index.js
+++ week-2/web-reactku/src/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const Hello = () => {
-  return <p>Hello!</p>
+const HelloComponent = () => {
+  return <p>Hello Component</p>
 }
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+class StateFullComponent extends React.Component {
+  render() {
+    return <p>StateFull Component</p>;
+  }
+}
 
-ReactDOM.render(<Hello />, document.getElementById("root"))
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<StateFullComponent />, document.getElementById("root"));
